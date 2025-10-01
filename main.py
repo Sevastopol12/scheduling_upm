1988871c7072cedd4d94bf65e5597088b5836bea
Sevastopol12 <chkalov253@gmail.com>
2025-10-01 12:27:32 +0700

    feat: add initial workflow with simulated annealing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from scheduling-upm!")
+from scheduling_upm.environment import (
+    generate_environment,
+    generate_schedule,
+    objective_function,
+)
+from scheduling_upm.simulated_annealing import simulated_annealing
 
+tasks, setups = generate_environment(n_tasks=4, n_machines=2)
+schedule = generate_schedule(tasks=tasks, n_machines=2)
+makespan = objective_function(schedule=schedule, tasks=tasks, setups=setups)
 
-if __name__ == "__main__":
-    main()
+print(simulated_annealing(tasks=tasks, setups=setups, n_machines=2))
--- scheduling_upm/environment.py
+++ scheduling_upm/environment.py
@@ -1,11 +1,10 @@
-from typing import List, Dict, Optional, Any
+from typing import List, Dict, Any, Tuple
 import random
-import numpy as np
 
 
 def generate_environment(n_tasks: int = 15, n_machines: int = 4) -> Dict[str, Any]:
     """Generate tasks, setup time of each task and precedence
-       Khởi tạo các task, thời gian setup và thứ tự ưu tiên cho từng task 
+       Khởi tạo các task, thời gian setup và thứ tự ưu tiên cho từng task
 
     Args:
         n_tasks (int, optional): number of task. Defaults to 15.
@@ -13,14 +12,16 @@ def generate_environment(n_tasks: int = 15, n_machines: int = 4) -> Dict[str, An
 
     Returns:
         tasks: dict: task_id -> {
-            "proc_times": time needed to perform this task on i'th machine: list[int], 
+            "proc_times": time needed to perform this task on i'th machine: list[int],
             Thời gian để thực hiện task này, khác nhau với mỗi máy
 
-            "preferred_start_time": int, 
+            "resource": int,
+            lượng tài nguyên cần để thực hiện task
+
             "weight": task importance: float
         }
         setup: dict of (task, task) of len 2 -> setup_time when changing task, differences for each pair of tasks
-        Thời gian setup để chuẩn bị cho task tiếp theo, setup time sẽ khác nhau với mỗi task 
+        Thời gian setup để chuẩn bị cho task tiếp theo, setup time sẽ khác nhau với mỗi task
         VD: setup_time từ task a->b, được biểu diễn là (a,b) sẽ khác setup_time từ task a->c (a,c) và ngược lại
 
         precedences: list of (task, task) denote that, say task a must be finished before task b starts
@@ -37,26 +38,26 @@ def generate_environment(n_tasks: int = 15, n_machines: int = 4) -> Dict[str, An
         for _ in range(n_machines):
             # Performance coefficient
             modifier: float = random.choice([0.6, 0.8, 1.0, 1.2, 1.6])
-            times.append(
-                max(1, int(base_proc[t] * modifier)))
+            times.append(max(1, int(base_proc[t] * modifier)))
 
-        preferred_start_time: int = random.randint(0, 120)
+        resource: int = random.randint(0, 120)
         weight: float = random.uniform(1.0, 3.0)
         tasks[t] = {
             "proc_times": times,
-            "preferred_start": preferred_start_time,
-            "weight": weight  # Task's importance
+            "resource": resource,
+            "weight": weight,  # Task's importance
         }
 
-    # Ý tưởng cho setup time và precedence, tạm thời lược bớt để đơn giản hóa khâu chuẩn bị objective function.
-    # Sẽ được chỉnh sửa và thêm lại từ t ừ
+    # Sequence-dependent setup times between tasks
+    setup_time = {}
+    for task_a in range(n_tasks):
+        for task_b in range(n_tasks):
+            setup_time[(task_a, task_b)] = (
+                0 if task_a == task_b else random.randint(0, 10)
+            )
 
-    # # Sequence-dependent setup times between tasks
-    # setup_time = {}
-    # for task_a in range(n_tasks):
-    #     for task_b in range(n_tasks):
-    #         setup_time[(task_a, task_b)
-    #                    ] = 0 if task_a == task_b else random.randint(0, 10)
+    # Ý tưởng cho precedence, tạm thời lược bớt để đơn giản hóa khâu chuẩn bị objective function.
+    # Sẽ được chỉnh sửa và thêm lại từ từ
 
     # # Precedence constraint
     # precedences = []
@@ -67,40 +68,46 @@ def generate_environment(n_tasks: int = 15, n_machines: int = 4) -> Dict[str, An
     #         precedences.append((task_a, task_b))
 
     # return tasks, setup_time, precedences
-    return tasks
+    return tasks, setup_time
 
 
-def generate_schedule(tasks: Dict[str, Any], n_machines: int = 4) -> Dict[int, List[int]]:
+def generate_schedule(
+    tasks: Dict[str, Any], n_machines: int = 4
+) -> Dict[int, List[int]]:
     """Perform Round-robin scheduling method to initialize schedule"""
-    schedule: Dict[int, List[int]] = {machine: []
-                                      for machine in range(n_machines)}
+    schedule: Dict[int, List[int]] = {machine: [] for machine in range(n_machines)}
 
     for task in tasks.keys():
         schedule[task % n_machines].append(task)
     return schedule
 
 
-def objective_function(schedule: Dict[int, List[int]], tasks: Dict[str, Any]):
-    # Calculate total-process time of each machine
-    machine_makespan: Dict[int, int] = {
-        machine: 0 for machine in schedule.keys()}
+def objective_function(
+    schedule: Dict[int, List[int]],
+    tasks: Dict[str, Any],
+    setups: Dict[Tuple[int, int], int],
+) -> Tuple:
+    """Objective: Minimize makespan"""
+    # Calculate total-process time of each machine. Accounts for setup time
+    machine_makespan: Dict[int, int] = {machine: 0 for machine in schedule.keys()}
 
     for machine, task_sequence in schedule.items():
-        for task in task_sequence:
-            machine_makespan[machine] += tasks[task]['proc_times'][machine]
-    return machine_makespan
+        for idx in range(len(task_sequence)):
+            # task's process time
+            task = task_sequence[idx]
+            process_time = tasks[task]["proc_times"][machine]
+            # setup time when changing task
+            setup_time = (
+                0 if idx < 1 else setups[task_sequence[idx - 1], task_sequence[idx]]
+            )
+            machine_makespan[machine] += process_time + setup_time
+
+    makespan = max(
+        [total_process_time for total_process_time in machine_makespan.values()]
+    )
+
+    return makespan
 
 
 if __name__ == "__main__":
-    tasks = generate_environment(n_tasks=4, n_machines=2)
-    schedule = generate_schedule(tasks=tasks, n_machines=2)
-    makespan = objective_function(schedule, tasks)
-
-    for key, item in tasks.items():
-        print(f"{key}: {item['proc_times']}\n")
-
-    for key, item in schedule.items():
-        print(f"{key}: {item}\n")
-
-    for key, item in makespan.items():
-        print(f"{key}: {item}\n")
+    generate_environment()
