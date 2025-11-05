682b2fa4441c44797559a37fd336dd4148017510
Sevastopol12 <chkalov253@gmail.com>
2025-11-05 23:17:48 +0700

    chore(precedence): adjust constraint logic

--- scheduling_upm/utils/evaluation.py
+++ scheduling_upm/utils/evaluation.py
@@ -9,16 +9,13 @@ def objective_function(
     precedences: Dict[int, Set[int]] = None,
 ) -> Tuple:
     """Objective: Minimize makespan"""
-    # Compute milestones of task's completion time .Accouns for setups
-    task_milestones, machine_milestones = record_milestones(
-        schedule=schedule, tasks=tasks, setups=setups
-    )
+    # Compute milestones of task's completion time .Accounts for setups
+    task_milestones = record_milestones(schedule=schedule, tasks=tasks, setups=setups)
 
     # Áp dụng ràng buộc precedences để tính thời gian hoàn thành thực tế của từng task
     if precedences is not None:
-        task_milestones, machine_milestones = apply_precedences_constraint(
+        task_milestones = apply_precedences_constraint(
             schedule=schedule,
-            machine_milestones=machine_milestones,
             task_milestones=task_milestones,
             precedences=precedences,
         )
@@ -27,7 +24,7 @@ def objective_function(
     # Áp dụng ràng buộc resource để tính thời gian hoàn thành thực tế của từng task
 
     # Makespan
-    makespan = compute_makespan(machine_milestones=machine_milestones)
+    makespan = compute_makespan(task_milestones=task_milestones)
 
     # TODO
     # Xét thêm những khía cạnh khác, tính cost
@@ -37,13 +34,9 @@ def objective_function(
     return cost
 
 
-def compute_makespan(machine_milestones: Dict[int, np.array]) -> int:
-    machine_completion_time: List[int] = [
-        milestones.max() if milestones.size > 0 else 0
-        for milestones in machine_milestones.values()
-    ]
-
-    return max(machine_completion_time)
+def compute_makespan(task_milestones: Dict[int, int]) -> int:
+    task_complete_time = [task_milestones[task]["complete_time"] for task in task_milestones.keys()]
+    return max(task_complete_time)
 
 
 def record_milestones(
@@ -58,7 +51,7 @@ def record_milestones(
     # E.g: { machine: [task_1_complete_time, task_2_complete_time,.etc],.etc }
 
     machine_milestones: Dict[int, List[int]] = {
-        machine: [] for machine in schedule.keys()
+        machine: 0 for machine in schedule.keys()
     }
 
     for machine, sequence in schedule.items():
@@ -69,38 +62,30 @@ def record_milestones(
             setup_time = 0 if idx < 1 else setups[sequence[idx - 1], sequence[idx]]
 
             # Start time
-            current_runtime: int = 0 if idx < 1 else machine_milestones[machine][-1]
+            current_runtime: int = 0 if idx < 1 else machine_milestones[machine]
 
             # Update new complete time
-            machine_milestones[machine].append(
-                current_runtime + process_time + setup_time
-            )
+            machine_milestones[machine] += current_runtime + process_time + setup_time
 
             # Store task info
             task_milestones[task] = {
-                "start_time": current_runtime,
-                "complete_time": machine_milestones[machine][-1],
+                "start_time": current_runtime + setup_time,
+                "complete_time": machine_milestones[machine],
                 "process_machine": machine,
                 "idx": idx,
             }
 
-    # Array transformation
-    machine_milestones = {
-        machine: np.array([sequence])
-        for machine, sequence in machine_milestones.items()
-    }
-    return task_milestones, machine_milestones
+    del machine_milestones
+    return task_milestones
 
 
 def apply_precedences_constraint(
     schedule: Dict[int, List[int]],
-    machine_milestones: Dict[int, np.array],
     task_milestones: Dict[int, int],
     precedences: Dict[int, Set[int]],
 ) -> Dict[int, int]:
     # Copy
     new_task_milestones = task_milestones
-    new_machine_milestones = machine_milestones
 
     # Storing machine total delay time as the result of taking precedences into accounts
     machine_delay_time = {}
@@ -117,25 +102,23 @@ def apply_precedences_constraint(
                 delay_time: int = precedence_complete_time - task_start_time
 
                 # Get task position
-                affected_machine_position: List[Tuple] = (
+                position: List[Tuple] = (
                     task_milestones[task]["process_machine"],
                     task_milestones[task]["idx"],
                 )
 
                 # Update the machine, position where the delay must be held
-                current_delay = machine_delay_time.get(affected_machine_position, 0)
-                machine_delay_time[affected_machine_position] = (
-                    current_delay + delay_time
-                )
+                current_delay = machine_delay_time.get(position, 0)
+                machine_delay_time[position] = current_delay + delay_time
 
     # Update milestones ( add delay time )
     for position, delay_time in machine_delay_time.items():
         # Get machine & affected index
-        machine, affected_idx = position
-        new_machine_milestones[machine][affected_idx:] += delay_time
+        machine, idx = position
 
-        for task in schedule[machine][affected_idx:]:
+        for task in schedule[machine][idx:]:
             task_milestones[task]["start_time"] += delay_time
             task_milestones[task]["complete_time"] += delay_time
 
-    return new_task_milestones, new_machine_milestones
+    del machine_delay_time
+    return new_task_milestones
