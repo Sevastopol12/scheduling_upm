e1ca14fd9cad1db17265aa039050f1d90a1e38b1
LinlinGin-dev <quydeptraihaha777@gmail.com>
2025-11-18 20:30:18 +0700

    chức năng kết hợp woa và sa, gọi chức năng mới trong main

--- main.py
+++ main.py
@@ -37,3 +37,24 @@ best_schedule, best_cost = solution.schedule, solution.cost
 print(best_cost)
 for machine, seq in best_schedule.items():
     print(f"{machine}: {seq}")
+
+
+best_solution_hybrid, total_time = hybrid_woa_sa(
+    tasks=tasks,
+    setups=setups,
+    precedences=precedences,
+    n_machines=n_machines,
+    n_schedules=10,        
+    n_iterations=100,      
+    sa_local_iters=2,      
+    seed=2503,
+    energy_constraint=energy_constraint,
+    total_resource=200,
+)
+
+best_schedule_hybrid, best_cost_hybrid = best_solution_hybrid.schedule, best_solution_hybrid.cost
+
+print("\n--- Hybrid WOA + SA ---")
+print(f"Best cost: {best_cost_hybrid}")
+for machine, seq in best_schedule_hybrid.items():
+    print(f"{machine}: {seq}")
